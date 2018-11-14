002b1c921a1768cf7c636245a1c3d9cde6fa9be5
3A532088 <paileo007007@gmail.com>
2018-11-14 11:28:22 +0800

    建立Model

--- database/migrations/2018_11_09_092319_add_is_feature_in_posts_table.php
+++ database/migrations/2018_11_09_092319_add_is_feature_in_posts_table.php
@@ -15,7 +15,7 @@ class AddIsFeatureInPostsTable extends Migration
     {
         Schema::table('posts', function (Blueprint $table) {
             //
-            $table->string('title', 500)->change();
+            $table->string('title', 506)->change();
             $table->boolean('is_feature')->default(false)->after('content');
         });
     }
