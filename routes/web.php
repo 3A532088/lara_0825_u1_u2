4bb07e35e36f732f1d8fc59e63bb3db6d93a31c9
3A532088 <paileo007007@gmail.com>
2018-11-20 20:59:21 +0800

    取得 Model

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,11 @@ Route::get('/', function () {
     $post->title='testtitle';
     $post->content='testcontent';
     $post->save();*/
-    $allPosts = \App\Post::all();
-    dd($allPosts);
-    $featuredPosts = \App\Post::where('is_feature', 1)->get();
-    dd($featuredPosts);
-    
+    $fourthPost = \App\Post::find(4);
+    dd($fourthPost);
+    $lastPost = \App\Post::orderBy('id', 'DESC')->first();
+    dd($lastPost);
+
 
 });
 
