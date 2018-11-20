8d985c9487de4ec2026ed4f4d348c69d7c8f1e5a
3A532088 <paileo007007@gmail.com>
2018-11-20 20:58:43 +0800

    取得 Collection

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,11 @@ Route::get('/', function () {
     $post->title='testtitle';
     $post->content='testcontent';
     $post->save();*/
-    \App\Post::destroy(3,5,7);
-
+    $allPosts = \App\Post::all();
+    dd($allPosts);
+    $featuredPosts = \App\Post::where('is_feature', 1)->get();
+    dd($featuredPosts);
+    
 
 });
 
